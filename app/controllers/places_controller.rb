5024a321ee82e5904337b95543097ea9715cc7b2
melmacmar <melmacmar@gmail.com>
2016-04-13 09:56:19 +0000

    Validation for Name field

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,10 +10,14 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
-  def create
-    current_user.places.create(place_params)
+def create
+  @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
   def show
      @place = Place.find(params[:id])
@@ -25,8 +29,17 @@ end
 
 def update
   @place = Place.find(params[:id])
+
+  if @place.user != current_user
+    return render text: 'Not Allowed', status: :forbidden
+  end
+
   @place.update_attributes(place_params)
-  redirect_to root_path
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
 end
 
 def destroy
@@ -37,11 +50,6 @@ def destroy
 
   @place.destroy
   redirect_to root_path
-end
-
-  @place.update_attributes(place_params)
-  redirect_to root_path
-
 end
 
   private
