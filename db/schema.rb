9d7c5a6b6f31c1e0addb2465b5d7ba2bcd8aa8a1
melmacmar <melmacmar@gmail.com>
2016-04-15 13:41:44 +0000

    Added photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413171815) do
+ActiveRecord::Schema.define(version: 20160415132657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 20160413171815) do
 
   add_index "installs", ["email"], name: "index_installs_on_email", unique: true, using: :btree
   add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true, using: :btree
+
+  create_table "photos", force: true do |t|
+    t.text     "caption"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "places", force: true do |t|
     t.string   "name"
