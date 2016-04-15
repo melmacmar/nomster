f73ee96f96ace0148ac14292d8bdcd3b4a26c542
kenmazaika <ken@thefirehoseproject.com>
2016-04-15 16:04:40 +0000

    fix broken photos.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415132657) do
+ActiveRecord::Schema.define(version: 20160415160320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160415132657) do
     t.text     "caption"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "place_id"
   end
 
   create_table "places", force: true do |t|
